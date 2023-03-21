15877d1670686addffe9382dd715152ef51f1b6a
devsync21 <devsync21@gmail.com>
2023-03-21 16:04:51 -0500

    winwinwin

--- src/components/Replies.js
+++ src/components/Replies.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useState,useEffect, useContext } from 'react'
 
-import { StyleSheet, Text, View, Button, FlatList,SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList,SafeAreaView, TouchableOpacity, TouchableWithoutFeedback  } from 'react-native';
 // import axios from "axios";
 import instance from '../api/Api';
+
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { SwipeRow } from 'react-native-swipe-list-view';
 
 import ThemeContext from '../store/ThemeContext';
 import ConfigContext from '../store/ConfigContext';
@@ -59,9 +62,11 @@ const Replies =  ({route}) => {
 
 
 
-  const ReplyCommonItem = (item) => {
-
+  const ReplyCommonItem = (item,rr) => {
+		
       const htmlDetail = (word) => {
+
+	
         if (word && word.length>0){
             return word.replace(/<br ?\/?>/ig,"\n")
         } else {
@@ -81,9 +86,21 @@ const Replies =  ({route}) => {
         <View >
             <View style = {{flexDirection:'row', justifyContent:'space-between', marginBottom:5}}>
                   {/* <Text >{item.item.rep_no}</Text>  */}
+
+				  
+				<View style = {{flexDirection:'row'}}>
+				  { item.item.rep_rep_icon === 0 ? 
+						<MaterialCommunityIcons name="arrow-right-bottom" style={{
+							color : themeValue.Reply.RsmFontColr,
+							fontSize: configValue.RcommonFontSize,
+							marginRight:5
+						}} /> : <Text></Text>
+					}
+
                   <Text style = {{fontSize:configValue.RcommonFontSize, marginLeft:0, 
 					color: themeValue.Reply.RsmFontColr}}>
 						{item.item.ip_address}</Text>   
+						</View>
                   <Text style = {{fontSize:configValue.RcommonFontSize, marginRight:5, 
 					color: themeValue.Reply.RsmFontColr}}>
 						{newdatetime}</Text>  
@@ -103,71 +120,112 @@ const Replies =  ({route}) => {
   
      
         return (
-            <TouchableOpacity
+            <TouchableWithoutFeedback 
             // onPress={this.itemClick(item)}
             // onPress =  () =>  console.log('dd')
             
        		>
 				<View style = {{   
 					borderTopWidth : 1,
-					paddingRight: 15,
+					// paddingRight: 15,
 					borderTopColor: themeValue.Reply.RborderColor}}>
 				
 					{ item.item.rep_rep_icon === 0 ?   
-						// 대댓글
-						<View style={{paddingLeft : 30, paddingTop:20, paddingBottom:20}}> 
-			
-							{ item.item.if_deleted.length === 0 ?
-								<ReplyCommonItem item = {item.item}/>
-								:
-								<View>
-									<Text style = {{color: themeValue.Reply.RdeletedFontColor}}> {item.item.if_deleted} </Text>
-								</View>
-							}
+
+						// 대댓글 시작
+						<View style={{ paddingLeft:30, paddingTop:10, paddingBottom:20, paddingRight: 15,
+						flexDirection:'row'
+
+						}}> 
 							
-						
+							<View>
+								{ item.item.if_deleted.length === 0 ?
+									// 삭제된 글이 아닌ㄹ 경우
+									<ReplyCommonItem item = {item.item} />
+									:
+									// 삭제된 글일 경우
+									<View>
+										<Text style = {{color: themeValue.Reply.RdeletedFontColor}}> {item.item.if_deleted} </Text>
+									</View>
+								}
+
+							</View>
 						</View> :
+						// 대댓글 끝
 
+						//  댓글   
 
-						//  댓글 
-						<View style={{paddingLeft : 10, paddingTop:20, paddingBottom:20}}> 
-						
-					
+						<View style={{paddingLeft : 10}}> 				
 							{ item.item.if_deleted.length === 0 ?
-								<ReplyCommonItem item = {item.item}/>
+
+
+								 // 스와이프 하면 댓글 옵션 나옴
+
+									// <ReplyCommonItem item = {item.item} />
+								    <SwipeRow rightOpenValue={-120}>
+										<View  style ={{
+											flexDirection:'row', 
+											justifyContent:'flex-end',
+											alignItems: 'center',
+											backgroundColor: '#8BC645',
+											flex: 1,
+											
+											padding: 15,
+										}}>
+											
+												<Text >답글달기</Text>
+									
+										
+										</View>
+										<View style = {{
+											  alignItems: 'center',
+											//   backgroundColor:  themeValue.Reply.RbackgroundColor,
+											backgroundColor:'red',
+											  justifyContent: 'center',
+											  paddingTop:10, paddingBottom:20, paddingRight: 15,
+											
+											}}>
+											<ReplyCommonItem item = {item.item} />
+										</View>
+									</SwipeRow>
+
+
+
 								:
 								<View>
 										<Text style = {{color:themeValue.Reply.RdeletedFontColor}}> {item.item.if_deleted} </Text>
 								</View>
-
 							}
-
 						</View>
 
 
 					}
 				</View>
-            </TouchableOpacity>
+            </TouchableWithoutFeedback >
         )
     
   } 
 
-// 메일 return
+// 메인 return
   return (
    
     <View  style = {{
-		flex:1,
-		backgroundColor: themeValue.Reply.RbackgroundColor
-	}}>
+        flex:1,
+        backgroundColor: themeValue.Reply.RbackgroundColor,
+		
+     		 }}>
 		<Text style = {{
-			color: themeValue.Reply.RnumfontColor
-		}}>댓글 {detailReply.length}</Text>
+			color: themeValue.Reply.RnumfontColor,
+			marginLeft:10,
+			marginBottom:3
+			
+			}}>댓글 {detailReply.length}</Text>
 
 
-        { detailReply && detailReply.length > 0 ? detailReply.map((item, id) => {
-         
-            return (<ReplyItem item={item} key={id}/>)
-        }) : ""}
+			{ detailReply && detailReply.length > 0 ? detailReply.map((item, id) => {
+			
+				return (<ReplyItem item={item} key={id}/>)
+			}) : ""}
    
       </View>
 
