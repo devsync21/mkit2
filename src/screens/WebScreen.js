a6876ead5b7a5fedbb2402efc6de2fca31206011
devsync21 <devsync21@gmail.com>
2023-03-28 14:47:03 -0500

    win again

--- src/screens/WebScreen.js
+++ src/screens/WebScreen.js
@@ -33,6 +33,10 @@ export default function WebScreen() {
     true; // note: this is required, or you'll sometimes get silent failures
  	 `;
 
+
+	const Cookie= "__qca=P0-1916131976-1671221339080; _ga=GA1.1.1065826753.1679943432; __gads=ID=b18992079a09125b:T=1679943431:S=ALNI_MZ9-V19F6wR6iCpptl0gO2Zkw5G7g; ASPSESSIONIDCQAASRQR=OECMOEPAPMFLJILAAMENLEEH; __gpi=UID=00000a3244a349bf:T=1679943431:RT=1680010172:S=ALNI_MbOI_ktg-24tDlXYThQDhdWaSCUqw; _ga_GZT8WBC5BM=GS1.1.1680010173.2.1.1680010473.0.0.0; _ga_HYVXTN0P7X=GS1.1.1680010879.2.0.1680010881.0.0.0; MissyUSA=secu=10c458b38111a8ed5023331920f3b393&MemberPermit=3&UserNick=%BD%C2%C8%F1%B8%BE&UserName=jinhee+kim&UserID=jhkim73&Login=loginok"
+
+
 	return (
 		<WebView
 		style={styles.container}
@@ -41,7 +45,7 @@ export default function WebScreen() {
 		injectedJavaScriptBeforeContentLoaded={runFirst}
 		source={{ uri: uri ,
 			headers: {
-				Cookie: "MissyUSA=secu=6202af00123804574717ee2497caa28a&MemberPermit=3&UserNick=%BD%C2%C8%F1%B8%BE&UserName=jinhee+kim&UserID=jhkim73&Login=loginok"
+				Cookie: Cookie
 			}
 		}}
 		// onLoadEnd={() => {
